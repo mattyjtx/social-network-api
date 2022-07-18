904d3f720f4f653764299d3d678b84c829353689
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-07-17 20:59:52 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
